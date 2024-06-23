0e97f6688e7cc54fb303da8bdfbc99929db8583f
Charity Rymbai <charityrymbai@hotmail.com>
2024-06-23 23:39:35 +0530

    updated Footer.jsx with grid making it responsive and good looking in small screen

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,25 +1,25 @@
 const Footer = () =>{
     return (
-        <div className="flex justify-center h-56 w-full bg-blue-900 bottom-0">
-            <div className="w-full lg:w-[50%] text-white flex justify-around items-center">
+        <div className="flex justify-center h-fit w-full bg-blue-900 bottom-0">
+            <div className="w-full p-5 grid content-center grid-cols-2 sm:w-fit sm:grid-cols-3 text-white">
                 <div>
-                    <h1 className="text-center">Contact</h1>
-                    <ul className="list-disc">
+                    <h1>Contact</h1>
+                    <ul className="list-disc p-5">
                         <li>school@edu.in</li>
                         <li>+91 1234567890</li>
                     </ul>
                 </div>
                 <div>
-                    <h1 className="text-center">Social</h1>
-                    <ul className="list-disc">
+                    <h1>Social</h1>
+                    <ul className="list-disc p-5">
                         <li>facebook</li>
                         <li>LinkIn</li>
                         <li>X</li>
                     </ul>
                 </div>
                 <div>
-                    <h1 className="text-center">Useful links</h1>
-                    <ul className="list-disc">
+                    <h1>Useful links</h1>
+                    <ul className="list-disc p-5">
                         <li>Downloads</li>
                         <li>Announcements</li>
                     </ul>
