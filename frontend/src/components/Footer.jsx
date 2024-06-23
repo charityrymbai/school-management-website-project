35e74070c081cf56e407809dc2a1fbec5ac5f3f8
Charity Rymbai <charityrymbai@hotmail.com>
2024-06-23 23:18:17 +0530

    updated Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,14 +3,14 @@ const Footer = () =>{
         <div className="flex justify-center h-56 w-full bg-blue-900 bottom-0">
             <div className="w-full lg:w-[50%] text-white flex justify-around items-center">
                 <div>
-                    <h1 className="text-[1.5rem] text-center">Contact</h1>
+                    <h1 className="text-center">Contact</h1>
                     <ul className="list-disc">
                         <li>school@edu.in</li>
                         <li>+91 1234567890</li>
                     </ul>
                 </div>
                 <div>
-                    <h1 className="text-[1.5rem] text-center">Social</h1>
+                    <h1 className="text-center">Social</h1>
                     <ul className="list-disc">
                         <li>facebook</li>
                         <li>LinkIn</li>
@@ -18,14 +18,12 @@ const Footer = () =>{
                     </ul>
                 </div>
                 <div>
-                    <h1 className="text-[1.5rem] text-center">Useful links</h1>
+                    <h1 className="text-center">Useful links</h1>
                     <ul className="list-disc">
                         <li>Downloads</li>
                         <li>Announcements</li>
                     </ul>
                 </div>
-                
-                
             </div>
         </div>
     )
