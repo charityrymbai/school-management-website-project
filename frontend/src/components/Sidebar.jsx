0e05d9a0288aa5ad0e5b4b3b3e4b837810ddd9f1
Charity Rymbai <charityrymbai@hotmail.com>
2024-07-09 20:06:31 +0530

    moving routes to single folder

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -10,7 +10,7 @@ export default function Sidebar({ children }) {
   const [expanded, setExpanded] = useState(true)
   
   return (
-    <aside className="h-screen w-fit">
+    <aside className="h-screen w-full sm:w-fit sm:z-50">
       <nav className="h-full flex flex-col bg-white border-r shadow-sm">
         <div className="p-4 pb-2 flex justify-between items-center">
           <img
@@ -101,7 +101,7 @@ export function SidebarItem({ icon, text, active, alert, goToPage}) {
           className={`
           absolute left-full rounded-md px-2 py-1 ml-6
           bg-indigo-100 text-indigo-800 text-sm
-          invisible opacity-20 -translate-x-3 transition-all
+          invisible -translate-x-3 transition-all
           group-hover:visible group-hover:opacity-100 group-hover:translate-x-0
       `}
         >
