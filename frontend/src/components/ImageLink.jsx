41d29842d7a65124fc764bda16bc07a5f82ecba1
Charity Rymbai <charityrymbai@hotmail.com>
2024-07-10 15:31:17 +0530

    centered text

--- frontend/src/components/ImageLink.jsx
+++ frontend/src/components/ImageLink.jsx
@@ -3,7 +3,7 @@ import { CardWrapper1 } from "../Wrappers/CardWrapper"
 const ImageLink = ({children, imageSrc})=>{
 
     return (
-        <div className="hover:cursor-pointer text-poppins text-2xl">
+        <div className="hover:cursor-pointer text-poppins text-2xl text-center">
             <CardWrapper1>
                 <img 
                     className="h-[200px]"
