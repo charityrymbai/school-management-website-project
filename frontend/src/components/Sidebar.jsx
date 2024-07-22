da4ab20b06665ea1a055b821cc1b20426c02a846
Charity Rymbai <charityrymbai@hotmail.com>
2024-07-22 17:36:28 +0530

    shown loader in LibraryPage and made sidebar vanish when data not arrived yet

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -3,12 +3,14 @@ import { useContext, createContext, useState, useEffect } from 'react';
 import { schoolLogo } from '../../public/images/photosExports';
 import { useBaseNavigate } from '../utils/useBaseNavigation.js';
 import { useLocation } from 'react-router-dom';
+import Loader from './Loader.jsx';
 
 const SidebarContext = createContext();
 
 export default function Sidebar({ children }) {
     const [expanded, setExpanded] = useState(true);
     const [showSidebar, setShowSidebar] = useState(false);
+    const [loading, setLoading] = useState(true);
 
     let [student, setStudent] = useState({});
 
@@ -32,75 +34,86 @@ export default function Sidebar({ children }) {
             })
             .then((data) => {
                 setStudent(data);
+                setLoading(false);
             });
     }, []);
 
     return (
         <>
-            <aside
-                className={`h-screen fixed w-full md:sticky md:w-fit top-0 z-50 bg-white border-r shadow-sm transition-transform transform ${
-                    showSidebar ? 'translate-x-0' : '-translate-x-full'
-                } md:translate-x-0`}
-            >
-                <nav className="h-full flex flex-col">
-                    <div className="p-4 pb-2 flex justify-center md:justify-between items-center">
-                        <img
-                            src={schoolLogo}
-                            className={`overflow-hidden transition-all ${
-                                expanded ? 'w-32' : 'w-0'
-                            }`}
-                            alt=""
-                        />
-                        <button
-                            onClick={() => setExpanded((curr) => !curr)}
-                            className="p-1.5 rounded-lg bg-gray-50 hover:bg-gray-100 hidden md:block"
-                        >
-                            {expanded ? <ChevronFirst /> : <ChevronLast />}
-                        </button>
-                    </div>
-
-                    <SidebarContext.Provider
-                        value={{ expanded, setShowSidebar }}
+            {loading ? (
+                <div></div>
+            ) : (
+                <div>
+                    <aside
+                        className={`h-screen fixed w-full md:sticky md:w-fit top-0 z-50 bg-white border-r shadow-sm transition-transform transform ${
+                            showSidebar ? 'translate-x-0' : '-translate-x-full'
+                        } md:translate-x-0`}
                     >
-                        <ul className="flex-1 px-3">{children}</ul>
-                    </SidebarContext.Provider>
+                        <nav className="h-full flex flex-col">
+                            <div className="p-4 pb-2 flex justify-center md:justify-between items-center">
+                                <img
+                                    src={schoolLogo}
+                                    className={`overflow-hidden transition-all ${
+                                        expanded ? 'w-32' : 'w-0'
+                                    }`}
+                                    alt=""
+                                />
+                                <button
+                                    onClick={() => setExpanded((curr) => !curr)}
+                                    className="p-1.5 rounded-lg bg-gray-50 hover:bg-gray-100 hidden md:block"
+                                >
+                                    {expanded ? (
+                                        <ChevronFirst />
+                                    ) : (
+                                        <ChevronLast />
+                                    )}
+                                </button>
+                            </div>
 
-                    <div className="border-t p-3 flex justify-center md:justify-between">
-                        <img
-                            src="https://ui-avatars.com/api/?background=c7d2fe&color=3730a3&bold=true"
-                            alt="Avatar"
-                            className="w-10 h-10 rounded-md"
-                        />
-                        <div
-                            className={`
+                            <SidebarContext.Provider
+                                value={{ expanded, setShowSidebar }}
+                            >
+                                <ul className="flex-1 px-3">{children}</ul>
+                            </SidebarContext.Provider>
+
+                            <div className="border-t p-3 flex justify-center md:justify-between">
+                                <img
+                                    src="https://ui-avatars.com/api/?background=c7d2fe&color=3730a3&bold=true"
+                                    alt="Avatar"
+                                    className="w-10 h-10 rounded-md"
+                                />
+                                <div
+                                    className={`
                 flex justify-center items-center text-center
                 md:justify-normal md:text-left
                 overflow-hidden transition-all ${expanded ? 'w-52 ml-3' : 'w-0'}
             `}
-                        >
-                            <div className="leading-4">
-                                <h4 className="font-semibold">{`${student.firstName} ${student.lastName}`}</h4>
-                                <span className="text-xs text-gray-600">
-                                    Class: {student.class}
-                                </span>
-                                <br />
-                                <span className="text-xs text-gray-600">
-                                    Roll no: {student.roll_no}
-                                </span>
+                                >
+                                    <div className="leading-4">
+                                        <h4 className="font-semibold">{`${student.firstName} ${student.lastName}`}</h4>
+                                        <span className="text-xs text-gray-600">
+                                            Class: {student.class}
+                                        </span>
+                                        <br />
+                                        <span className="text-xs text-gray-600">
+                                            Roll no: {student.roll_no}
+                                        </span>
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                    </div>
-                </nav>
-            </aside>
-            <button
-                onClick={() => {
-                    setShowSidebar((curr) => !curr);
-                    setExpanded(true);
-                }}
-                className="fixed top-5 right-5 md:hidden p-2 bg-white rounded-full shadow-lg z-50"
-            >
-                <Menu size={24} />
-            </button>
+                        </nav>
+                    </aside>
+                    <button
+                        onClick={() => {
+                            setShowSidebar((curr) => !curr);
+                            setExpanded(true);
+                        }}
+                        className="fixed top-5 right-5 md:hidden p-2 bg-white rounded-full shadow-lg z-50"
+                    >
+                        <Menu size={24} />
+                    </button>
+                </div>
+            )}
         </>
     );
 }
