83804fc51b9cdb4304fbca97e4bf702ee8793c1b
Charity Rymbai <charityrymbai@hotmail.com>
2024-07-30 16:30:45 +0530

    adding overview, search student and searchTeacher pages

--- frontend/src/Routes/AdminRoutes.jsx
+++ frontend/src/Routes/AdminRoutes.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { Route, Routes, Navigate } from 'react-router-dom';
+const SearchTeacher = React.lazy(() => import('../pages/admin/SearchTeacher'));
+const SearchStudent = React.lazy(() => import('../pages/admin/SearchStudent'));
+const Overview = React.lazy(() => import('../pages/admin/Overview'));
 const Student = React.lazy(() => import('../pages/admin/Student'));
 const Teacher = React.lazy(() => import('../pages/admin/Teacher'));
 
@@ -7,7 +10,10 @@ const AdminRoutes = () => {
     return (
         <>
             <Routes>
-                <Route path="" element={<Navigate to="student" />} />
+                <Route path="" element={<Navigate to="overview" />} />
+                <Route path="overview" element={<Overview />} />
+                <Route path="searchStudent" element={<SearchStudent />} />
+                <Route path="searchTeacher" element={<SearchTeacher />} />
                 <Route path="student" element={<Student />} />
                 <Route path="teacher" element={<Teacher />} />
             </Routes>
---
+++ frontend/src/pages/admin/Overview.jsx
@@ -0,0 +1,11 @@
+import TopBar2 from '../../components/TopBar2';
+
+const Overview = () => {
+    return (
+        <div className="w-full">
+            <TopBar2 Heading={'Overview'} />
+        </div>
+    );
+};
+
+export default Overview;
